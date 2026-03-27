3779170b6d1ed97e19069595267467764f15da98
Sahil191202 <kingkhan120702@gmail.com>
2026-03-27 17:19:40 +0530

    Complete Backend

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ import { connectDB } from "./src/config/db.js";
 
 connectDB().then(() => {
   app.listen(ENV.PORT, () => {
-    console.log(`Server running on port ${ENV.PORT}`);
+    console.log(`✅Server running on port ${ENV.PORT}`);
   });
 }).catch((err) => {
   console.error("MongoDB connection failed!", err);
